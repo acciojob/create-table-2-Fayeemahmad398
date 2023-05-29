3cfe2759f8e0c500358aedfd7543997ff0b91a29
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-05-29 13:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
     //Write your code here
+	let rn=prompt("Input number of rows");
+	let cn=prompt("Input number of columns");
+	let table=document.getElementById("myTable");
+	for(let i=0;i<=rn;i++)
+		{
+			let row=document.createElement("tr");
+			for(let j=0;j<=cn;j++){
+				let td=document.createElement("td");
+				td.textContent=`Row-${i} Column-${j}`
+				row.appendChild(td);
+			}
+			table.appendChild(row);
+		}
+
+	
   
 }
